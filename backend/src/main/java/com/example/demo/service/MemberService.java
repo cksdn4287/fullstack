0b05231df3c443caf38f8867df8cddd57de53127
cksdn4287 <cksdn4287@gmail.com>
2026-02-25 17:53:19 +0900

    카카오 로그인 설정중423

--- backend/src/main/java/com/example/demo/service/MemberService.java
+++ backend/src/main/java/com/example/demo/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
+import java.util.stream.Collectors;
+
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.domain.Member;
 import com.example.demo.dto.MemberDTO;
 
 @Transactional
@@ -9,4 +12,17 @@ public interface MemberService {
 
 	
 	MemberDTO getKakaoMember(String accessToken);
+	
+	default MemberDTO entityToDTO(Member member) {
+		
+		MemberDTO dto = new MemberDTO(	
+				member.getEmail(),
+				member.getPw(),
+				member.getNickname(),
+				member.isSocial(),
+				member.getMemberRoleList().stream().map(memberRole -> memberRole.name()).collect(Collectors.toList()));
+		
+		return dto;
+				
+	}
 }
